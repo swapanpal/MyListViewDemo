6a4e6dd90350f711187e5770c1343ca3084956e1
swapan <swapan.d83@gmail.com>
2020-03-30 12:37:15 +0700

    setOnItemClickListener added to listView

--- app/src/main/java/com/example/mylistviewdemo/MainActivity.java
+++ app/src/main/java/com/example/mylistviewdemo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mylistviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
 For listView
@@ -12,9 +15,9 @@ For listView
  * (multiple textView with image or audio etc)will use BaseAdapter
  1. need a listView xml
  2. Data source (array, Array list, database
- 3. a view (adapter collect data from data source and paste it on that view than set it
+ 3. a view where adapter initially put data from data source than set it on listView
+ 4. adapter (adapter collect data from data source and paste it on that view than set it
     on list view.
- 4. adapter
  */
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
@@ -23,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // find the view
         mListView = findViewById(R.id.list_view);
-        String[] countryName = getResources().getStringArray(R.array.country_names);
-
+        // pull data from string to mainActivity
+        final String[] countryName = getResources().getStringArray(R.array.country_names);
+        // Create a ArrayAdapter to pull and set data from datasource to listView via a view
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.text_view,countryName);
+        // set adapter in listView
         mListView.setAdapter(adapter);
+           // set a click listener on the listView
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String value = countryName[i];
+                Toast.makeText(getApplicationContext(), value + " position" + i,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
